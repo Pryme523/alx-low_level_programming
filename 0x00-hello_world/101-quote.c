514532c34e2715c8b9e302176db2838d073bd3e0
Pryme523 <boakyeemmanuel523@gmail.com>
2024-05-11 08:35:04 -0700

    add from assember to qoute.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints a string
+ *
+ * Return 0 Always
+*/
+
+int main(void) 
+
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar,2015-10-19",59);
+return (1);
+}
